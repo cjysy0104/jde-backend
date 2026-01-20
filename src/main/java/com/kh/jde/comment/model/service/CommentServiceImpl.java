b7dd68fad9ed7003ccaedb52473f19b86ca334de
yoosh0610 <yoo31318@gmail.com>
2026-01-20 10:07:59 +0900

    refactor:프로필 변경시 S3Service 사용, feat:기본 프로필 이미지 선택(변경)

--- src/main/java/com/kh/jde/comment/model/service/CommentServiceImpl.java
+++ src/main/java/com/kh/jde/comment/model/service/CommentServiceImpl.java
@@ -100,32 +100,5 @@ public class CommentServiceImpl implements CommentService {
 		
 		commentValidator.validateResult("댓글 수정", commentMapper.update(param));
 	}
-
-	@Override
-	public int update(Long commentNo, CustomUserDetails principal, CommentDTO request) {
-
-		// 1. 댓글 잇냐
-		CommentDTO comment = commentMapper.existsComment(commentNo);
-		if(comment == null) {
-			throw new PostNotFoundException("댓글이 존재하지 않습니다.");
-		}
-		if("N".equals(comment.getStatus())) {
-			throw new IllegalArgumentException("이미 삭제된 댓글입니다.");
-		}
-		// 2. 니꺼냐?
-		if(!(comment.getMemberNo().equals(principal.getMemberNo()))) {
-			throw new AccessDeniedException("댓글 삭제할 권한이 없습니다.");
-		}
-		// 3. 댓글 수정
-		comment.setContent(request.getContent());
-		
-		int result = commentMapper.update(comment);
-		if(result != 1) {
-			throw new IllegalArgumentException("수정 실패했습니다.");
-		}
-		return result;
-	}
-	
-	
 	
 }
