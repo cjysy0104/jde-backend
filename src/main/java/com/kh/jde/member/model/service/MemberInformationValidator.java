0a62dd291406e24ae0bf70d46e97c1425f189597
Ahnjunyoung927 <younge64@naver.com>
2026-01-22 09:32:44 +0900

    feat: 이메일 중복검사, 닉네임 중복검사, fix: 오타수정, 사용하지않는 메서드 삭제

--- src/main/java/com/kh/jde/member/model/service/MemberInformationValidator.java
+++ src/main/java/com/kh/jde/member/model/service/MemberInformationValidator.java
@@ -14,26 +14,9 @@ public class MemberInformationValidator {
 	
 	private final MemberMapper memberMapper;
 	
-	public void MemberInfomationDuplicateCheck(String nickname, String email, String phone) {
-		int nicknameCount = memberMapper.countByNickname(nickname);
-		if(1 <= nicknameCount) {
-			throw new MemberInfomationDuplicatedException("이미 존재하는 닉네임입니다.");
-		}
-		
-		int EamilCount = memberMapper.countByEmail(email);
-		if(1 <= EamilCount) {
-			throw new MemberInfomationDuplicatedException("이미 존재하는 이메일입니다.");
-		}
-		
-		int PhoneCount = memberMapper.countByPhone(phone);
-		if(1 <= PhoneCount) {
-			throw new MemberInfomationDuplicatedException("이미 존재하는 휴대전화 번호입니다.");
-		} 	
-	}
-	
 	public void emailDuplicateCheck(String email) {
-		int EamilCount = memberMapper.countByEmail(email);
-		if(1 <= EamilCount) {
+		int EmailCount = memberMapper.countByEmail(email);
+		if(1 <= EmailCount) {
 			throw new MemberInfomationDuplicatedException("이미 존재하는 이메일입니다.");
 		}
 	}
