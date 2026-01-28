7fbe5ba80704231622e19a5616e45d25e7799d2f
yoosh0610 <yoo31318@gmail.com>
2026-01-28 15:12:06 +0900

    feat:신고기능

--- src/main/java/com/kh/jde/report/model/service/ReportService.java
+++ src/main/java/com/kh/jde/report/model/service/ReportService.java
@@ -1,7 +1,10 @@
 package com.kh.jde.report.model.service;
 
+import java.util.List;
+
 import com.kh.jde.report.model.dto.CommentReportCreateDTO;
 import com.kh.jde.report.model.dto.ReviewReportCreateDTO;
+import com.kh.jde.report.model.vo.ReportCategoryVO;
 
 public interface ReportService {
 	
@@ -10,4 +13,7 @@ public interface ReportService {
 	
 	// 리뷰 신고 등록
 	void createReviewReport(ReviewReportCreateDTO dto);
+	
+	// 신고 카테고리
+	List<ReportCategoryVO> getReportCategories();
 }
